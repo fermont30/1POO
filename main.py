89241659cc945ff6abba07dfd4a46fc0f69b9f91
santiago gullichico <sdq.guallichico@yavirac.edu.ec>
2022-11-30 13:59:57 -0500

    ejemplo

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 print ("Hola Mundo")
-..
-
+ 
+ class persona:
+     nombre = str
+     edad = int
+     
+     def __init__ (self, nombre, edad):
+         self.nombre  = nombre
+         self.edad  = edad
+         
+     def saluda (self, otra_persona)    
+        return 'hola {otra_persona.nombre}, me llamo {self.nombre}.'
+         
+     if __name__ == "__main__"      
+        david = persona("david", 30) 
+        maria = persona("maria", 20)
+        
+        print(david)
+        print(maria)   
+         
